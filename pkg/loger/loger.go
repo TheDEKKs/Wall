d5e4a0d71ac17a348453ba71f855313d238ee4d9
thedekks <thedekkegor@gmail.com>
2025-10-08 22:36:50 +0300

    Work Version Log in File

--- pkg/loger/loger.go
+++ pkg/loger/loger.go
@@ -3,27 +3,51 @@ package pkg
 import (
 	"log"
 	"os"
+
+	"go.uber.org/zap"
 )
 
-var Log *log.Logger
-var file *os.File
+var Zap *zap.Logger
 
 func InitLoger(file_log string) error{
-
 	var err error
-	file, err = os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-
+	file, err := os.OpenFile(file_log, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil{
 		log.Println("Error create file ", err)
 		return err
 	}
+
+	defer file.Close()
+	
+
+	loger := zap.NewProductionConfig()
+	loger.OutputPaths = []string{"stdout", "./log/.log"}
+
+	Zap, err = loger.Build()
+
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+
+	defer Zap.Sync()
+
+	Zap.Info("StartApp")
+	return nil
+
+	/*path, _ := os.Executable()var err error
+
+	log.Println(file, "Test", path+file_log)
+
 	defer file.Close()
 
-	Logs := log.New(file, "ERROR: ", log.LstdFlags)
+	log.SetOutput(file)
 
-
-	Logs.Println("Good create")
+	Log = log.New(file, "ERROR: ", log.LstdFlags)
 
 	
+	Log.Println("Test")
+	
 	return nil
+	*/
 }
