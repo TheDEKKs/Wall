d97b248b4151a344e8ed3e99b0b20205de639525
thedekks <thedekkegor@gmail.com>
2026-01-27 00:40:09 +0300

    feat: create service

--- internal/transport/bot/server.go
+++ internal/transport/bot/server.go
@@ -1,13 +1,41 @@
 package bot
 
 import (
+	"fmt"
 	"thedekk/WWT/internal/domains/bot"
 	"thedekk/WWT/internal/transport/bot/handlers"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func StartBot(conn *pgxpool.Pool) {
+func StartBot(updates tgbotapi.UpdatesChannel, botAPI *tgbotapi.BotAPI, conn *pgxpool.Pool) {
 	botService := bot.NewBotService(conn)
 	botHandler := handlers.NewBotHandler(botService)
+	fmt.Printf("botHandler: %v\n", botHandler)
+
+	for up := range updates {
+		if up.Message != nil && up.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(up.Message.Chat.ID, "")
+			msg.ParseMode = "Markdown"
+			
+			switch up.Message.Command(){
+			case "post":
+				msg.Text = "Your registration cod -"
+
+
+			default:
+				continue
+
+			}
+			if _, err := botAPI.Send(msg); err != nil {
+				fmt.Println(err)
+			}
+
+			continue
+
+		}
+	
+	}
 }
